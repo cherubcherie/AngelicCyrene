7d3220c0bfc8cebc261de7ddb824321b4214eea2
cherubcherie <baylaprincessghaile@gmail.com>
2026-09-06 10:40:22 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -20,8 +20,8 @@ export const botConfig = {
     // 2 = Listening
     // 3 = Watching
     // 4 = Custom
-    // 5 = Competing
-   activities: [
+    // 5 = Competinga
+activities: [
         {
             name: "Cherubim Community", // required by Discord API, not shown in the client
             state: "The goat Cherry is my Master", // this is what people actually see
